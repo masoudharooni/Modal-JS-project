4ccede744f7dda83fc8e30ea050010312b7bc274
masoud harooni <masoudharooni50@gmail.com>
2022-10-22 20:50:30 +0330

    Close the modal with ESC keyboard button

--- script.js
+++ script.js
@@ -3,21 +3,25 @@ const modal = document.querySelector('.modal');
 const closeModalBtn = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay')
 const openModalBtns = document.querySelectorAll('button.show-modal');
-console.log(openModalBtns);
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
 // Display modal logic
 for (let i = 0; i < openModalBtns.length; i++) {
-    openModalBtns[i].addEventListener('click', function () {
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
+    openModalBtns[i].addEventListener('click', openModal);
 }
 // close modal logic
-closeModalBtn.addEventListener('click', function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-});
+closeModalBtn.addEventListener('click', closeModal);
 // close modal when clicked outside of it
-overlay.addEventListener('click', function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+overlay.addEventListener('click', closeModal);
+// close modal with ESC keyboard button
+document.addEventListener('keydown', function (e) {
+    if (e.code === 'Escape') {
+        closeModal();
+    }
 });
