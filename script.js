dd0d195d17e2351d1ac7a965129af128b2cddcd2
masoud harooni <masoudharooni50@gmail.com>
2022-10-22 19:47:48 +0330

    Declare project variables

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay')
+const openModalBtns = document.querySelectorAll('.show-modal');
